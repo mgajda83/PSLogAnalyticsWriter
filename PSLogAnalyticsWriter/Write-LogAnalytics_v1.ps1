eb988d0d6375c8face2a80a225e73a1cfacef9d3
Michal Gajda <47522674+mgajda83@users.noreply.github.com>
2026-05-08 09:50:51 +0200

    Add files via upload

---
+++ PSLogAnalyticsWriter/Write-LogAnalytics_v1.ps1
@@ -0,0 +1,107 @@
+﻿Function Write-LogAnalytics_v1
+{
+<#
+	.SYNOPSIS
+		Write object to LogAnalytics workspace using HTTP Data Collector API (deprecated)
+		The Azure Monitor HTTP Data Collector API has been deprecated and will no longer be functional as of 9/14/2026
+
+	.PARAMETER WorkspaceID
+		WorkspaceID of LogAnalytics workspace.
+
+	.PARAMETER SharedKey
+		SharedKey of LogAnalytics workspace.
+
+	.PARAMETER Object
+		PowerShell object to write to LogAnalytics workspace
+
+	.PARAMETER Table
+		Custom table name in LogAnalytics workspace.
+
+	.PARAMETER TimeGeneratedField
+		Optional name of a field that includes the timestamp for the data.
+
+	.EXAMPLE
+		$WorkspaceID = "<GUID WorkspaceID>"
+		$SharedKey = "<SharedKey>"
+		$Object = [PSCustomObject]@{
+			Requester    = $env:USERNAME
+			ComputerName = $env:COMPUTERNAME
+			Id           = (New-Guid).Guid
+			Message      = "Custom Message"
+		}
+
+		$Log = @{
+			WorkspaceID = $WorkspaceID
+			SharedKey = $SharedKey
+			Object = $Object
+			Table = "CustomLog"
+		}
+		Write-LogAnalytics @Log
+
+	.NOTES
+		Author: Michal Gajda
+	#>
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$WorkspaceID,
+		[Parameter(Mandatory=$true)][String]$SharedKey,
+		[Parameter(Mandatory=$true,ValueFromPipeline=$True)]$Object,
+		[Parameter()][String]$Table = "CustomLog",
+		[Parameter()][String]$TimeGeneratedField = ""
+	)
+
+	Begin {}
+
+	Process
+	{
+		if($Object -is [String]) { $Json = $Object } else { $Json = $Object | ConvertTo-Json -Compress }
+		$Body = [System.Text.Encoding]::UTF8.GetBytes($Json)
+
+		#Sign params
+		$Method = "POST"
+		$ContentType = "application/json"
+		$ContentLength = $Body.Length
+		$SignDate = [DateTime]::UtcNow.ToString("r")
+		$xHeaders = "x-ms-date:" + $SignDate
+		$APIResource = "/api/logs"
+
+		#String to Sign
+		$StringToSign = $Method + "`n" + $ContentLength + "`n" + $ContentType + "`n" + $xHeaders + "`n" + $APIResource
+		$BytesToHash = [Text.Encoding]::UTF8.GetBytes($StringToSign)
+		$KeyBytes = [Convert]::FromBase64String($SharedKey)
+		$HMACSHA256 = New-Object System.Security.Cryptography.HMACSHA256
+		$HMACSHA256.Key = $KeyBytes
+		$CalculatedHash = $HMACSHA256.ComputeHash($BytesToHash)
+		$EncodedHash = [Convert]::ToBase64String($CalculatedHash)
+		$Authorization = 'SharedKey {0}:{1}' -f $WorkspaceID, $EncodedHash
+
+		#Request params
+		$Uri = "https://" + $WorkspaceID + ".ods.opinsights.azure.com" + $APIResource + "?api-version=2016-04-01"
+		$Headers = @{
+			"Authorization" = $Authorization
+			"Log-Type" = $Table
+			"x-ms-date" = $SignDate
+			"time-generated-field" = $TimeGeneratedField
+		}
+
+		$Request = @{
+			Uri = $Uri
+			Method = $Method
+			ContentType = "application/json"
+			Headers = $Headers
+			Body = $Body
+			UseBasicParsing = $true
+		}
+
+		#Send request
+		$Response = Invoke-WebRequest @Request
+
+		if ($Response.StatusCode -eq 200)
+		{
+			Write-Verbose -Message "Event was write to Log Analytics Workspace"
+		}
+	}
+
+	End {}
+}
---
+++ PSLogAnalyticsWriter/Write-LogAnalytics_v2.ps1
@@ -0,0 +1,93 @@
+﻿Function Write-LogAnalytics_v2
+{
+	<#
+	.SYNOPSIS
+		Write object to LogAnalytics workspace using Logs Ingestion API.
+
+	.PARAMETER AccessToken
+		AccessToken for authorization header.
+
+	.PARAMETER DCEUri
+		Data collection endpoint logs ingestion Url.
+
+	.PARAMETER DCRId
+		Data collection rule id	(immutableId)
+
+	.PARAMETER Table
+		Custom table name in LogAnalytics workspace.
+
+	.PARAMETER Object
+		PowerShell object to write to LogAnalytics workspace.
+
+	.EXAMPLE
+		#Get token
+		$Params = @{
+			Scopes = 'https://monitor.azure.com/.default'
+			Secret = $Secret
+			TenantId = $TenantId
+			ClientId = $ApplicationId
+			AzureCloudInstance = "AzurePublic"
+		}
+		$Token = Get-PSMSALToken @Params
+
+		#Create object to send
+		$Object = [PSCustomObject]@{
+			Requester    = $env:USERNAME
+			ComputerName = $env:COMPUTERNAME
+			Id           = (New-Guid).Guid
+			Message      = "Custom Message"
+		}
+
+		#Write to Log Analytics
+		$Log = @{
+			AccessToken = $Token.AccessToken
+			DCEUri = "https://test-data-collection-endpoint.westeurope-1.ingest.monitor.azure.com"
+			DCRId = "dcr-abcdef1234567890"
+			Table = "CustomLog"
+			Object = $Object
+		}
+		Write-LogAnalytics @Log
+
+	.NOTES
+		Author: Michal Gajda
+	#>
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$AccessToken,
+		[Parameter(Mandatory=$true)][String]$DCEUri,
+		[Parameter(Mandatory=$true)][String]$DCRId,
+		[Parameter(Mandatory=$true)][String]$Table,
+		[Parameter(Mandatory=$true)]$Object
+	)
+
+	#Create Authorization headers
+	$Headers = @{
+		Authorization = "Bearer $AccessToken"
+		"Content-Type" = "application/json"
+	}
+
+	#Convert Object to JSON Array
+	if($Host.Version -gt [Version]"7.2")
+	{
+		Write-Verbose "Converting object to JSON using -AsArray parameter."
+		$Body = $Object | ConvertTo-Json -Depth 5 -Compress -AsArray
+	} else {
+		#Fix for PS5.1
+		if($Object -is [System.Array])
+		{
+			Write-Verbose "The object is an array."
+			$Body = $Object | ConvertTo-Json -Depth 5 -Compress
+		} else {
+			Write-Verbose "The object is not an array. Manual converting to JSON array."
+			$Body = "[" + $($Object | ConvertTo-Json -Depth 5 -Compress) + "]"
+		}
+	}
+
+	#Write to Log Analytics
+	$Uri = "$($DCEUri)/dataCollectionRules/$($DCRId)/streams/Custom-$($Table)?api-version=2023-01-01"
+	Write-Verbose "URI: $Uri"
+	Write-Verbose "Body: $Body"
+
+	Invoke-RestMethod -Uri $Uri -Method POST -Body $Body -Headers $Headers
+}
